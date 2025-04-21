2f333b279d0ad2295f970eed2d5c11f35b15b6e3
Miguel Armas <marmasa@proveedor.intercorp.com.pe>
2025-04-21 07:08:37 -0500

    Add package-info

--- rules-processor-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
+++ rules-processor-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
@@ -21,16 +21,16 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public enum ErrorDictionary {
 
   //system=00
-  INVALID_FIELD("11.00.01", "Invalid field", BAD_REQUEST, InvalidFieldException.class),
-  NO_SUCH_REST_CLIENT("11.00.02", "No such rest client", INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
-  ERROR_READING_JSON("11.00.03", "Error reading JSON", INTERNAL_SERVER_ERROR, JsonReadException.class),
-  ERROR_MAPPING_REFLECTIVE_PARAMS("11.00.04", "Error mapping reflective params", INTERNAL_SERVER_ERROR, ReflectiveParamMappingException.class),
-  ERROR_ASSIGN_REFLECTIVE_PARAMS("11.00.05", "Error assign reflective params", INTERNAL_SERVER_ERROR, ReflectiveParamAssignmentException.class),
-  EMPTY_BASE_URL("11.00.06", "Base URL is required", INTERNAL_SERVER_ERROR, EmptyBaseUrlException.class),
+  INVALID_FIELD("01.00.01", "Invalid field", BAD_REQUEST, InvalidFieldException.class),
+  NO_SUCH_REST_CLIENT("01.00.02", "No such rest client", INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
+  ERROR_READING_JSON("01.00.03", "Error reading JSON", INTERNAL_SERVER_ERROR, JsonReadException.class),
+  ERROR_MAPPING_REFLECTIVE_PARAMS("01.00.04", "Error mapping reflective params", INTERNAL_SERVER_ERROR, ReflectiveParamMappingException.class),
+  ERROR_ASSIGN_REFLECTIVE_PARAMS("01.00.05", "Error assign reflective params", INTERNAL_SERVER_ERROR, ReflectiveParamAssignmentException.class),
+  EMPTY_BASE_URL("01.00.06", "Base URL is required", INTERNAL_SERVER_ERROR, EmptyBaseUrlException.class),
 
   //custom=01
-  ERROR_COMPILING_RULES("11.01.01", "Error compiling rules", INTERNAL_SERVER_ERROR, RulesCompilationException.class),
-  NO_SUCH_RULE_MAPPER("11.01.02", "No such rule mapper", INTERNAL_SERVER_ERROR, NoSuchRuleMapperException.class);
+  ERROR_COMPILING_RULES("01.01.01", "Error compiling rules", INTERNAL_SERVER_ERROR, RulesCompilationException.class),
+  NO_SUCH_RULE_MAPPER("01.01.02", "No such rule mapper", INTERNAL_SERVER_ERROR, NoSuchRuleMapperException.class);
 
   private final String code;
   private final String message;
