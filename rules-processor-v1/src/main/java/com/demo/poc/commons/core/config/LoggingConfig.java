b55b39641e9f164962ab7314d37d6500de80c135
Miguel Armas <marmasa@proveedor.intercorp.com.pe>
2025-04-26 11:00:31 -0500

    Add unit tests

--- rules-processor-v1/src/main/java/com/demo/poc/commons/core/config/LoggingConfig.java
+++ rules-processor-v1/src/main/java/com/demo/poc/commons/core/config/LoggingConfig.java
@@ -1,8 +1,6 @@
 package com.demo.poc.commons.core.config;
 
 import com.demo.poc.commons.core.logging.ErrorThreadContextInjector;
-import com.demo.poc.commons.core.logging.RestClientThreadContextInjector;
-import com.demo.poc.commons.core.logging.RestServerThreadContextInjector;
 import com.demo.poc.commons.core.logging.ThreadContextInjector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,16 +16,6 @@ public class LoggingConfig {
   @Bean
   public ErrorThreadContextInjector threadContextErrorInjector(ThreadContextInjector context) {
     return new ErrorThreadContextInjector(context);
-  }
-
-  @Bean
-  public RestClientThreadContextInjector threadContextRestClientInjector(ThreadContextInjector context) {
-    return new RestClientThreadContextInjector(context);
-  }
-
-  @Bean
-  public RestServerThreadContextInjector threadContextRestServerInjector(ThreadContextInjector context) {
-    return new RestServerThreadContextInjector(context);
   }
 
 }
