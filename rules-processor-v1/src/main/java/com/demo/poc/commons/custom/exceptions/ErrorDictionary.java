da5c5c9e6b3475612bff9e2a1b82e78d3fcb559b
Miguel <miguel.armas.abt@gmail.com>
2025-05-04 18:18:31 -0500

    Upgrade Core Library

--- rules-processor-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
+++ rules-processor-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
@@ -1,6 +1,5 @@
 package com.demo.poc.commons.custom.exceptions;
 
-import com.demo.poc.commons.core.errors.dto.ErrorType;
 import com.demo.poc.commons.core.errors.exceptions.EmptyBaseUrlException;
 import com.demo.poc.commons.core.errors.exceptions.GenericException;
 import com.demo.poc.commons.core.errors.exceptions.InvalidFieldException;
@@ -10,12 +9,10 @@ import com.demo.poc.commons.core.errors.exceptions.NoSuchRestClientErrorExtracto
 import com.demo.poc.commons.core.errors.exceptions.NoSuchRestClientException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.util.Arrays;
 
-import static com.demo.poc.commons.core.errors.dto.ErrorType.BUSINESS;
-import static com.demo.poc.commons.core.errors.dto.ErrorType.SYSTEM;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -24,26 +21,25 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public enum ErrorDictionary {
 
   //system=01.00.xx
-  ERROR_READING_JSON("01.00.01", "Error reading JSON", SYSTEM, INTERNAL_SERVER_ERROR, JsonReadException.class),
+  ERROR_READING_JSON("01.00.01", "Error reading JSON", INTERNAL_SERVER_ERROR, JsonReadException.class),
 
   //no such properties and components: 01.01.xx
-  NO_SUCH_REST_CLIENT("01.01.01", "No such rest client", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
-  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("01.01.02", "No such rest client error extractor", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
-  NO_SUCH_PARAM_MAPPER("01.01.03", "No such param mapper", SYSTEM, BAD_REQUEST, NoSuchParamMapperException.class),
-  EMPTY_BASE_URL("01.01.04", "Base URL is required", SYSTEM, INTERNAL_SERVER_ERROR, EmptyBaseUrlException.class),
-  NO_SUCH_RULE_MAPPER("01.01.05", "No such rule mapper", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRuleMapperException.class),
+  NO_SUCH_REST_CLIENT("01.01.01", "No such rest client", INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
+  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("01.01.02", "No such rest client error extractor", INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
+  NO_SUCH_PARAM_MAPPER("01.01.03", "No such param mapper", INTERNAL_SERVER_ERROR, NoSuchParamMapperException.class),
+  EMPTY_BASE_URL("01.01.04", "Base URL is required", INTERNAL_SERVER_ERROR, EmptyBaseUrlException.class),
+  NO_SUCH_RULE_MAPPER("01.01.05", "No such rule mapper", INTERNAL_SERVER_ERROR, NoSuchRuleMapperException.class),
 
   //business and bad requests: 01.02.xx
-  INVALID_FIELD("01.02.01", "Invalid field", BUSINESS, BAD_REQUEST, InvalidFieldException.class),
-  ERROR_COMPILING_RULES("01.01.01", "Error compiling rules", SYSTEM, INTERNAL_SERVER_ERROR, RulesCompilationException.class),
+  INVALID_FIELD("01.02.01", "Invalid field", BAD_REQUEST, InvalidFieldException.class),
+  ERROR_COMPILING_RULES("01.01.01", "Error compiling rules", INTERNAL_SERVER_ERROR, RulesCompilationException.class),
 
   //transaction limits: 01.03.xx
-  NO_SUCH_TRANSFER_LIMIT("01.03.01", "No such transfer limit", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchTransferLimitException.class);
+  NO_SUCH_TRANSFER_LIMIT("01.03.01", "No such transfer limit", INTERNAL_SERVER_ERROR, NoSuchTransferLimitException.class);
 
   private final String code;
   private final String message;
-  private final ErrorType type;
-  private final HttpStatus httpStatus;
+  private final HttpStatusCode httpStatus;
   private final Class<? extends GenericException> exceptionClass;
 
   public static ErrorDictionary parse(Class<? extends GenericException> exceptionClass) {
